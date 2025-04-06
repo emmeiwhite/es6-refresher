5472e106270a5425efcc0b7f5f06a3289bb4c130
emmeiwhite <emmeiwhite@gmail.com>
2025-04-06 13:43:00 +0530

    es6-1.1 let |  syntax error

--- script.js
+++ script.js
@@ -5,4 +5,65 @@ function example() {
 }
 
 example() // 10
-console.log(x) // ReferenceError: x is not defined
+
+try {
+  console.log(x) // ReferenceError: x is not defined
+} catch (error) {
+  console.log('Error Handled gracefully')
+}
+
+// var: Reassignment & redeclaration of var | Can lead to issues if not used correctly
+
+/** a) Reassigning  */
+var k = 10
+console.log(k) // 10
+
+k = 'mayank'
+console.log(k) // mayank
+
+/** var: Redefinition with var */
+var isRaining = true
+console.log(isRaining)
+
+var isRaining = 'No, it is not raining'
+console.log(isRaining)
+
+/** 2. let
+ * In JavaScript, the let keyword is used to declare variables that are block-scoped, meaning they are only accessible within the block in which they are defined. Block scope refers to any code block delimited by curly braces {} such as loops, conditionals, or function bodies.
+ */
+
+let x = 10
+if (true) {
+  let y = 20
+  console.log(x) // Output: 10
+  console.log(y) // Output: 20
+}
+console.log(x) // Output: 10
+try {
+  console.log(y) // Throws ReferenceError: y is not defined
+} catch (error) {
+  console.log('Risky code handled, prevented ReferenceError')
+}
+
+/**
+ * Reassignment and Redefinition using let
+Variables declared with let can be reassigned, just like those declared with var, but they cannot be redeclared within the same block scope. This helps prevent accidental redeclaration of variables and can aid in writing more predictable and maintainable code.
+ */
+
+let preparation = true
+console.log(preparation)
+
+preparation = 'setting up the deadlines' // re-assigning it but we cannot re-declare within the same scope, which helps prevent unnecessary bugs
+console.log(preparation)
+
+/** Let's try to re-declare a variable with let and see the outcome */
+// Re definition using 'let'
+
+let foo = 10
+
+// let foo =
+//     'Mayank';
+/** Trying to re-declare foo results in a syntax error, which stops whole program execution, in fact it occurs during the compile time even in partially compiled languages like javaScript */
+console.log(foo) // Output : Error;
+
+const hello; // Again a syntax error which results in termination of JS Program
